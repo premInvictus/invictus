06619bd3aa93026563d662fc6a9b6794dbfea88c
Shikhar <utslara1@gmail.com>
2019-05-09 10:24:01 +0530

    student removed from axiom

--- projects/axiom/src/app/reports/student/student-performance-graph/student-performance-graph.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component} from '@angular/core';
-
-@Component({
-	selector: 'app-student-performance-graph',
-	templateUrl: './student-performance-graph.component.html',
-	styleUrls: ['./student-performance-graph.component.css']
-})
-export class StudentPerformanceGraphComponent {
-}
