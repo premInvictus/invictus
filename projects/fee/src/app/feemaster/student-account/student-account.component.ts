cc8ca17ec17fc363fde450f78cdd2121523dded1
Shikhar <utslara1@gmail.com>
2019-05-13 12:57:02 +0530

    edit req done

--- projects/fee/src/app/feemaster/student-account/student-account.component.ts
+++ projects/fee/src/app/feemaster/student-account/student-account.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { CommonAPIService, ProcesstypeService, FeeService } from '../../_services';
 import { DatePipe } from '@angular/common';
 import { ConfirmValidParentMatcher } from '../../_validationclass/confirmValidParentMatcher.class';
@@ -32,10 +32,14 @@ export class StudentAccountComponent implements OnInit, OnChanges {
 	hostelStatus: any;
 	existFlag = false;
 	hostelTerminateFlag = false;
-	@Input() editFlag = false;
-	@Input() permissionFlag = false;
+	@ViewChild('editModal') editModal;
+	@Input() viewOnly = true;
 	@Input() feeLoginId: any;
 	@Output() editChange = new EventEmitter();
+	finalSibReqArray: any[] = [];
+	finalArray: any[] = [];
+	accountDetails: any = {};
+	reqObj: any = {};
 	constructor(
 		private fbuild: FormBuilder,
 		private feeService: FeeService,
@@ -59,7 +63,7 @@ export class StudentAccountComponent implements OnInit, OnChanges {
 	}
 	ngOnChanges() {
 		if (this.feeLoginId) {
-		this.getFeeAccount(this.feeLoginId);
+			this.getFeeAccount(this.feeLoginId);
 		}
 	}
 	buildForm() {
@@ -91,63 +95,62 @@ export class StudentAccountComponent implements OnInit, OnChanges {
 		});
 	}
 	getFeeAccount(au_login_id) {
-		let accountDetails: any = {};
 		this.feeService.getFeeAccount({ accd_login_id: au_login_id }).subscribe((result: any) => {
 			if (result && result.status === 'ok') {
 				this.existFlag = true;
-				accountDetails = result.data[0];
-				if (accountDetails.accd_is_transport === 'Y') {
+				this.accountDetails = result.data[0];
+				if (this.accountDetails.accd_is_transport === 'Y') {
 					this.transportFlag = true;
 				} else {
 					this.transportFlag = false;
 				}
-				if (accountDetails.accd_tr_id === '1') {
+				if (this.accountDetails.accd_tr_id === '1') {
 					this.modeFlag = true;
 				} else {
 					this.modeFlag = false;
 				}
-				if (accountDetails.accd_is_terminate === 'Y') {
+				if (this.accountDetails.accd_is_terminate === 'Y') {
 					this.terminationFlag = true;
 					this.terminateStatus = 'Transport Facility Terminated';
 				} else {
 					this.terminationFlag = false;
 					this.terminateStatus = 'Transport Facility Available';
 				}
-				if (accountDetails.accd_is_hostel_terminate === 'Y') {
+				if (this.accountDetails.accd_is_hostel_terminate === 'Y') {
 					this.hostelTerminateFlag = true;
 					this.hostelStatus = 'Hostel Facility Terminated';
 				} else {
 					this.hostelTerminateFlag = false;
 					this.hostelStatus = 'Hostel Facility Available';
 				}
-				if (accountDetails.accd_is_hostel === 'Y') {
+				if (this.accountDetails.accd_is_hostel === 'Y') {
 					this.hostelFlag = true;
 				} else {
 					this.hostelFlag = false;
 				}
 				this.accountsForm.patchValue({
-					accd_id: accountDetails.accd_id,
-					accd_login_id: accountDetails.accd_login_id,
-					accd_fo_id: accountDetails.accd_fo_id,
-					accd_fs_id: accountDetails.accd_fs_id,
-					accd_fcg_id: accountDetails.accd_fcg_id,
-					accd_is_transport: accountDetails.accd_is_transport === 'N' ? false : true,
-					accd_is_hostel: accountDetails.accd_is_hostel === 'N' ? false : true,
-					accd_transport_mode: accountDetails.accd_transport_mode,
-					accd_tr_id: accountDetails.accd_tr_id,
-					accd_tsp_id: accountDetails.accd_tsp_id,
-					accd_ts_id: accountDetails.accd_ts_id,
-					accd_is_terminate: accountDetails.accd_is_terminate === 'N' ? false : true,
-					accd_is_hostel_terminate: accountDetails.accd_is_hostel_terminate === 'N' ? false : true,
-					accd_transport_from: accountDetails.accd_transport_from,
-					accd_transport_to: accountDetails.accd_transport_to,
-					accd_remark: accountDetails.accd_remark,
-					accd_hostel_fs_id: accountDetails.accd_hostel_fs_id,
-					accd_hostel_fcc_id: accountDetails.accd_hostel_fcc_id,
-					accd_hostel_from: accountDetails.accd_hostel_from,
-					accd_hostel_to: accountDetails.accd_hostel_to,
-					accd_ses_id: accountDetails.ses_id,
-					accd_status: accountDetails.accd_status
+					accd_id: this.accountDetails.accd_id,
+					accd_login_id: this.accountDetails.accd_login_id,
+					accd_fo_id: this.accountDetails.accd_fo_id,
+					accd_fs_id: this.accountDetails.accd_fs_id,
+					accd_fcg_id: this.accountDetails.accd_fcg_id,
+					accd_is_transport: this.accountDetails.accd_is_transport === 'N' ? false : true,
+					accd_is_hostel: this.accountDetails.accd_is_hostel === 'N' ? false : true,
+					accd_transport_mode: this.accountDetails.accd_transport_mode,
+					accd_tr_id: this.accountDetails.accd_tr_id,
+					accd_tsp_id: this.accountDetails.accd_tsp_id,
+					accd_ts_id: this.accountDetails.accd_ts_id,
+					accd_is_terminate: this.accountDetails.accd_is_terminate === 'N' ? false : true,
+					accd_is_hostel_terminate: this.accountDetails.accd_is_hostel_terminate === 'N' ? false : true,
+					accd_transport_from: this.accountDetails.accd_transport_from,
+					accd_transport_to: this.accountDetails.accd_transport_to,
+					accd_remark: this.accountDetails.accd_remark,
+					accd_hostel_fs_id: this.accountDetails.accd_hostel_fs_id,
+					accd_hostel_fcc_id: this.accountDetails.accd_hostel_fcc_id,
+					accd_hostel_from: this.accountDetails.accd_hostel_from,
+					accd_hostel_to: this.accountDetails.accd_hostel_to,
+					accd_ses_id: this.accountDetails.ses_id,
+					accd_status: this.accountDetails.accd_status
 				});
 			} else {
 				this.commonAPIService.showSuccessErrorMessage(result.message, 'error');
@@ -330,19 +333,9 @@ export class StudentAccountComponent implements OnInit, OnChanges {
 				if (result && result.status === 'ok') {
 					this.commonAPIService.showSuccessErrorMessage('Inserted SucessFully', 'success');
 					this.getFeeAccount(this.feeLoginId);
-					if (this.permissionFlag) {
-						this.editFlag = false;
-						} else {
-							this.editFlag = true;
-						}
-					this.editChange.emit(this.editFlag);
+					this.editChange.emit(true);
 				} else {
-					if (this.permissionFlag) {
-						this.editFlag = false;
-						} else {
-							this.editFlag = true;
-						}
-					this.editChange.emit(this.editFlag);
+					this.editChange.emit(true);
 				}
 			});
 		}
@@ -411,25 +404,20 @@ export class StudentAccountComponent implements OnInit, OnChanges {
 				accd_hostel_to: datePipe.transform(this.accountsForm.value.accd_hostel_to, 'yyyy-MM-dd'),
 				accd_status: this.accountsForm.value.accd_status
 			};
-			this.feeService.updateFeeAccount(accountJSON).subscribe((result: any) => {
-				if (result && result.status === 'ok') {
-					this.commonAPIService.showSuccessErrorMessage('Updated SucessFully', 'success');
-					this.getFeeAccount(this.feeLoginId);
-					if (this.permissionFlag) {
-						this.editFlag = false;
-						} else {
-							this.editFlag = true;
-						}
-					this.editChange.emit(this.editFlag);
-				} else {
-					if (this.permissionFlag) {
-						this.editFlag = false;
-						} else {
-							this.editFlag = true;
-						}
-					this.editChange.emit(this.editFlag);
-				}
-			});
+			if (this.isExist('350')) {
+				this.feeService.updateFeeAccount(accountJSON).subscribe((result: any) => {
+					if (result && result.status === 'ok') {
+						this.commonAPIService.showSuccessErrorMessage('Updated SucessFully', 'success');
+						this.getFeeAccount(this.feeLoginId);
+						this.editChange.emit(true);
+					} else {
+						this.editChange.emit(true);
+					}
+				});
+			}
+			if (this.isExist('374')) {
+				this.checkFormChangedValue();
+			}
 		}
 	}
 	next(admno) {
@@ -454,14 +442,70 @@ export class StudentAccountComponent implements OnInit, OnChanges {
 	}
 	getPreviousData() {
 		this.getFeeAccount(this.feeLoginId);
-		if (this.permissionFlag) {
-			this.editFlag = false;
-			} else {
-				this.editFlag = true;
-			}
-		this.editChange.emit(this.editFlag);
+		this.viewOnly = true;
+		this.editChange.emit(true);
 	}
 	isExist(mod_id) {
 		return this.commonAPIService.isExistUserAccessMenu(mod_id);
+	}
+	confirmEdit($event) {
+		if ($event) {
+			this.viewOnly = true;
+			this.editChange.emit(true);
+		}
+	}
+	cancelEdit($event) {
+		if ($event) {
+			this.viewOnly = true;
+			this.editChange.emit(true);
+		}
+	}
+	checkFormChangedValue() {
+		this.finalSibReqArray = [];
+		this.finalArray = [];
+		const datepipe = new DatePipe('en-US');
+		const sibReqArray: any[] = [];
+		Object.keys(this.accountsForm.value).forEach((key: any) => {
+			const formControl = <FormControl>this.accountsForm.controls[key];
+			if (formControl.dirty) {
+				if (key === 'accd_transport_from' || key === 'accd_transport_to'
+					|| key === 'accd_hostel_to' || key === 'accd_hostel_from') {
+					sibReqArray.push({
+						rff_where_id: 'accd_id',
+						rff_where_value: this.accountDetails['accd_id'],
+						rff_field_name: key,
+						rff_new_field_value: new DatePipe('en-in').transform(formControl.value, 'yyyy-MM-dd'),
+						rff_old_field_value: this.accountDetails[key],
+					});
+				} else {
+					sibReqArray.push({
+						rff_where_id: 'accd_id',
+						rff_where_value: this.accountDetails['accd_id'],
+						rff_field_name: key,
+						rff_new_field_value: formControl.value,
+						rff_old_field_value: this.accountDetails[key],
+					});
+				}
+			}
+		});
+		this.finalSibReqArray.push({ item: sibReqArray });
+		for (const sib of this.finalSibReqArray) {
+			for (const titem of sib.item) {
+				this.finalArray.push(titem);
+			}
+		}
+		this.reqObj = {
+			req_login_id: JSON.parse(localStorage.getItem('currentUser')).login_id,
+			req_process_type: '4',
+			req_tab_id: '4',
+			req_priority: '',
+			req_remarks: '',
+			req_reason: '',
+			req_date: datepipe.transform(new Date, 'yyyy-MM-dd'),
+			req_param: []
+		};
+		if (this.finalArray.length > 0) {
+			this.editModal.openModal({ data: [this.finalArray], reqParam: [this.reqObj] });
+		}
 	}
 }
