e6fc4cef606179bf1851aae7402b1df6d7d8dd61
shadmanwaris <shadman@invictusdigisoft.com>
2019-06-12 19:47:02 +0530

    ui updated

--- projects/smart/src/app/syllabus/review-syllabus/review-syllabus.component.ts
+++ projects/smart/src/app/syllabus/review-syllabus/review-syllabus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-review-syllabus',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewSyllabusComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogRef = this.dialog.open(EditSyllabus, {
+      height: '550px',
+      width: '700px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 
   ngOnInit() {
   }
 
 }
+
+@Component({
+  selector: 'edit-syllabus',
+  templateUrl: 'edit-syllabus.html',
+})
+export class EditSyllabus {}
