d2773b4d90a3b2093d43a2505c09f66cd4e5a017
shadmanwaris <shadman@invictusdigisoft.com>
2019-06-13 15:23:32 +0530

    ui updated

--- projects/smart/src/app/logentry/topicwise-update/topicwise-update.component.ts
+++ projects/smart/src/app/logentry/topicwise-update/topicwise-update.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-topicwise-update',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopicwiseUpdateComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
+
+  openUpdateConfirmation() {
+    const dialogRef = this.dialog.open(UpdateConfirmation, {
+      height: '400px',
+      width: '550px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 
   ngOnInit() {
   }
 
 }
+
+@Component({
+  selector: 'update-confirmation',
+  templateUrl: 'update-confirmation.html',
+})
+export class UpdateConfirmation {}
