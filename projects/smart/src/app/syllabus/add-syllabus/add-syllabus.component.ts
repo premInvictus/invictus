9626b7f11ffcf479f14e7991a13638d7eb0fb406
Shashank <shashank@invictusdigisoft.com>
2019-06-15 18:51:22 +0530

    Smart Details Page

--- projects/smart/src/app/syllabus/add-syllabus/add-syllabus.component.ts
+++ projects/smart/src/app/syllabus/add-syllabus/add-syllabus.component.ts
@@ -10,13 +10,16 @@ import { SyllabusserviceService } from './../syllabusservice.service';
 export class AddSyllabusComponent implements OnInit {
 			public parameterform: FormGroup;
 			public parameterform2: FormGroup;
+			activityUpdateFlag = false;
+			syllabusUpdateFlag = false;
+			syllabusvalue1: any;
+			syllabusvalue2: any;
 			modalForm: FormGroup;
 			public classArray: any[];
 			public termArray: any[];
 			public ctrArray: any[];
 			public topicArray: any[];
-			finalAwardArray: any[] = [];
-			finalAwardArray2: any[] = [];
+			finalSyllabusArray: any[] = [];
 			finalSpannedArray: any[] = [];
 			currentUser: any;
 			syl_id: any;
@@ -31,6 +34,10 @@ export class AddSyllabusComponent implements OnInit {
 			syllabus_flag = true;
 			details_flag = false;
 			editRequestFlag = false;
+			totalByTopic: any[] = [{
+				topic_id: '',
+				total: 0
+			}];
 			constructor(
 				private fbuild: FormBuilder,
 				private syllabusservice: SyllabusserviceService,
@@ -183,10 +190,24 @@ export class AddSyllabusComponent implements OnInit {
 				return this.termArray[tindex].term_name;
 			}
 		}
+		getCtrName(value) {
+			for (const item of this.ctrArray) {
+				if (item.ctr_id === value) {
+					return item.ctr_name;
+				}
+			}
+		}
 		getTopicName(value) {
 			for (const item of this.topicArray) {
 				if (item.topic_id === value) {
 					return item.topic_name;
+				}
+			}
+		}
+		getSubTopicName(value) {
+			for (const item of this.subtopicArray) {
+				if (Number(item.st_id) === Number(value)) {
+					return item.st_name;
 				}
 			}
 		}
@@ -219,39 +240,153 @@ export class AddSyllabusComponent implements OnInit {
 				this.finalSpannedArray = [];
 			}
 			if (this.parameterform2.valid) {
-				this.finalAwardArray.push(this.parameterform2.value);
-				for (let i = 0; i < this.finalAwardArray.length; i++) {
+				this.finalSyllabusArray.push(this.parameterform2.value);
+
+				for (let i = 0; i < this.finalSyllabusArray.length; i++) {
+					let sd_period_teacher: any = '';
+					let sd_period_test: any = '';
+					let sd_period_revision: any = '';
+
+					if (this.finalSyllabusArray[i].sd_ctr_id === '1') {
+						sd_period_teacher = this.finalSyllabusArray[i].sd_period_req;
+					} else if (this.finalSyllabusArray[i].sd_ctr_id === '2') {
+						sd_period_test = this.finalSyllabusArray[i].sd_period_req;
+					} else {
+						sd_period_revision = this.finalSyllabusArray[i].sd_period_req;
+					}
 					const spannArray: any[] = [];
 					spannArray.push({
-						sd_topic_id: this.finalAwardArray[i].sd_topic_id,
-						sd_st_id: this.finalAwardArray[i].sd_st_id,
-						sd_period_req: this.finalAwardArray[i].sd_period_req,
-						sd_ctr_id: this.finalAwardArray[i].sd_ctr_id,
-						sd_desc: this.finalAwardArray[i].sd_desc,
+						sd_topic_id: this.finalSyllabusArray[i].sd_topic_id,
+						sd_st_id: this.finalSyllabusArray[i].sd_st_id,
+						sd_period_req: this.finalSyllabusArray[i].sd_period_req,
+						sd_period_teacher: sd_period_teacher,
+						sd_period_test: sd_period_test,
+						sd_period_revision: sd_period_revision,
+						sd_ctr_id: this.finalSyllabusArray[i].sd_ctr_id,
+						sd_desc: this.finalSyllabusArray[i].sd_desc,
 					});
-					for (let j = i + 1; j < this.finalAwardArray.length; j++) {
-						if (this.finalAwardArray[i].sd_topic_id === this.finalAwardArray[j].sd_topic_id) {
+					for (let j = i + 1; j < this.finalSyllabusArray.length; j++) {
+						let sd_period_teacher1: any = '';
+						let sd_period_test1: any = '';
+						let sd_period_revision1: any = '';
+						if (this.finalSyllabusArray[i].sd_topic_id === this.finalSyllabusArray[j].sd_topic_id) {
+							if (this.finalSyllabusArray[j].sd_ctr_id === '1') {
+								sd_period_teacher1 = this.finalSyllabusArray[j].sd_period_req;
+							} else if (this.finalSyllabusArray[j].sd_ctr_id === '2') {
+								sd_period_test1 = this.finalSyllabusArray[j].sd_period_req;
+							} else {
+								sd_period_revision1 = this.finalSyllabusArray[j].sd_period_req;
+							}
 							spannArray.push({
-								sd_topic_id: this.finalAwardArray[i].sd_topic_id,
-								sd_st_id: this.finalAwardArray[j].sd_st_id,
-								sd_period_req: this.finalAwardArray[j].sd_period_req,
-								sd_ctr_id: this.finalAwardArray[j].sd_ctr_id,
-								sd_desc: this.finalAwardArray[j].sd_desc,
+								sd_topic_id: this.finalSyllabusArray[i].sd_topic_id,
+								sd_st_id: this.finalSyllabusArray[j].sd_st_id,
+								sd_period_req: this.finalSyllabusArray[j].sd_period_req,
+								sd_period_teacher: sd_period_teacher1,
+								sd_period_test: sd_period_test1,
+								sd_period_revision: sd_period_revision1,
+								sd_ctr_id: this.finalSyllabusArray[j].sd_ctr_id,
+								sd_desc: this.finalSyllabusArray[j].sd_desc,
 							});
 						}
 					}
-					const findex = this.finalSpannedArray.findIndex(f => f.sd_topic_id === this.finalAwardArray[i].sd_topic_id);
+					const findex = this.finalSpannedArray.findIndex(f => f.sd_topic_id === this.finalSyllabusArray[i].sd_topic_id);
 					if (findex === -1) {
 						this.finalSpannedArray.push({
-							sd_topic_id: this.finalAwardArray[i].sd_topic_id,
-							details: spannArray
+							sd_topic_id: this.finalSyllabusArray[i].sd_topic_id,
+							details: spannArray,
+							total : this.finalSyllabusArray[i].sd_period_req
 						});
+					} else {
+						this.finalSpannedArray[findex].total = this.finalSpannedArray[findex].total + this.finalSyllabusArray[i].sd_period_req;
 					}
+					console.log(this.finalSpannedArray);
 				}
 				this.resetForm();
 			} else {
 				this.common.showSuccessErrorMessage('Please fill all required fields', 'error');
 			}
-			console.log(this.finalAwardArray);
 		}
-}
+		editSyllabusList(value1, value2) {
+			this.syllabusUpdateFlag = true;
+			this.syllabusvalue1 = value1;
+			this.syllabusvalue2 = value2;
+			this.parameterform2.patchValue({
+				'sd_ctr_id': this.finalSpannedArray[value1].details[value2].sd_ctr_id,
+				'sd_topic_id': this.finalSpannedArray[value1].sd_topic_id,
+				'sd_st_id': this.finalSpannedArray[value1].details[value2].sd_st_id,
+				'sd_period_req': this.finalSpannedArray[value1].details[value2].sd_period_req,
+				'sd_desc': this.finalSpannedArray[value1].details[value2].sd_desc,
+			});
+		}
+		updateSyllabussList() {
+			const findex = this.finalSyllabusArray.findIndex(f => f.sd_topic_id === this.finalSpannedArray[this.syllabusvalue1].sd_topic_id
+				&& f.sd_ctr_id === this.finalSpannedArray[this.syllabusvalue1].details[this.syllabusvalue2].sd_ctr_id
+				&& f.sd_period_req === this.finalSpannedArray[this.syllabusvalue1].details[this.syllabusvalue2].sd_period_req);
+			this.finalSyllabusArray[findex] = this.parameterform2.value;
+			const spannArray: any[] = [];
+				let sd_period_teacher2: any = '';
+				let sd_period_test2: any = '';
+				let sd_period_revision2: any = '';
+				if (this.parameterform2.value.sd_ctr_id === '1') {
+					sd_period_teacher2 = this.parameterform2.value.sd_period_req;
+				} else if (this.parameterform2.value.sd_ctr_id === '2') {
+					sd_period_test2 = this.parameterform2.value.sd_period_req;
+				} else {
+					sd_period_revision2 = this.parameterform2.value.sd_period_req;
+				}
+// tslint:disable-next-line: max-line-length
+				this.finalSpannedArray[this.syllabusvalue1].total = this.finalSpannedArray[this.syllabusvalue1].total - this.finalSpannedArray[this.syllabusvalue1].details[this.syllabusvalue2].sd_period_req;
+			if (this.parameterform2.value.sd_topic_id === this.finalSpannedArray[this.syllabusvalue1].sd_topic_id) {
+				this.finalSpannedArray[this.syllabusvalue1].sd_topic_id = this.parameterform2.value.sd_topic_id;
+				this.finalSpannedArray[this.syllabusvalue1].details[this.syllabusvalue2] = this.parameterform2.value;
+				this.finalSpannedArray[this.syllabusvalue1].details[this.syllabusvalue2].sd_period_teacher = sd_period_teacher2;
+				this.finalSpannedArray[this.syllabusvalue1].details[this.syllabusvalue2].sd_period_test = sd_period_test2;
+				this.finalSpannedArray[this.syllabusvalue1].details[this.syllabusvalue2].sd_period_revision = sd_period_revision2;
+// tslint:disable-next-line: max-line-length
+				this.finalSpannedArray[this.syllabusvalue1].total = this.finalSpannedArray[this.syllabusvalue1].total + this.parameterform2.value.sd_period_req;
+			} else {
+				spannArray.push({
+					sd_ctr_id: this.parameterform2.value.sd_ctr_id,
+					sd_topic_id: this.parameterform2.value.sd_topic_id,
+					sd_period_req: this.parameterform2.value.sd_period_req,
+					sd_period_teacher: sd_period_teacher2,
+					sd_period_test: sd_period_test2,
+					sd_period_revision: sd_period_revision2,
+					sd_st_id: this.parameterform2.value.sd_st_id,
+					sd_desc: this.parameterform2.value.sd_desc,
+				});
+				console.log(this.finalSpannedArray);
+				this.finalSpannedArray[this.syllabusvalue1].details.splice(this.syllabusvalue2, 1);
+
+				const rindex = this.finalSpannedArray.findIndex(f => f.sd_topic_id === this.parameterform2.value.sd_topic_id);
+				if (rindex === -1) {
+					this.finalSpannedArray.push({
+						sd_topic_id: this.parameterform2.value.sd_topic_id,
+						details: spannArray
+						//total : this.finalSyllabusArray[i].sd_period_req
+					});
+				} else {
+					this.finalSpannedArray[rindex].details = spannArray;
+				}
+
+				// this.finalSpannedArray.push({
+				// 	sd_topic_id: this.parameterform2.value.sd_topic_id,
+				// 	details: spannArray
+				// });
+				// console.log(this.finalSpannedArray);
+				// //this.addDetailsList();
+			}
+			this.common.showSuccessErrorMessage('Syllabus List Updated', 'success');
+			this.resetForm();
+			this.syllabusUpdateFlag = false;
+		}
+		deleteSyllabusList(value1, value2) {
+			if (this.finalSpannedArray[value1].details.length > 1) {
+				this.finalSpannedArray[value1].details.splice(value2, 1);
+			} else {
+				this.finalSpannedArray.splice(value1, 1);
+			}
+			this.finalSyllabusArray.splice(value1, 1);
+			this.resetForm();
+		}
+} 
