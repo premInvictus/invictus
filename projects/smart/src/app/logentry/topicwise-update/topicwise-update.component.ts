e5a887e9860b1467e3c8caea4b53b21ee2e834b0
Bablu Shaw <bablushaw1988@gmail.com>
2019-06-18 15:58:18 +0530

    topicwise update top selection portion done

--- projects/smart/src/app/logentry/topicwise-update/topicwise-update.component.ts
+++ projects/smart/src/app/logentry/topicwise-update/topicwise-update.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
+import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
+import { AxiomService, SisService, SmartService, CommonAPIService } from '../../_services';
 import { UpdateConfirmationComponent } from './update-confirmation/update-confirmation.component';
 
 @Component({
@@ -9,7 +11,21 @@ import { UpdateConfirmationComponent } from './update-confirmation/update-confir
 })
 export class TopicwiseUpdateComponent implements OnInit {
 
-	constructor(public dialog: MatDialog) {}
+	topicwiseforForm: FormGroup;
+	subjectArray: any[] = [];
+	categoryArray: any[] = [];
+	classArray: any[] = [];
+	sectionArray: any[] = [];
+	classSectionArray: any[] = [];
+	teacherId = '';
+	constructor(
+		private fbuild: FormBuilder,
+		private axiomService: AxiomService,
+		private sisService: SisService,
+		private smartService: SmartService,
+		private commonAPIService: CommonAPIService,
+		public dialog: MatDialog
+		) {}
 
 	openUpdateConfirmation() {
 		const dialogRef = this.dialog.open(UpdateConfirmationComponent, {
@@ -23,6 +39,84 @@ export class TopicwiseUpdateComponent implements OnInit {
 	}
 
 	ngOnInit() {
+		this.buildForm();
 	}
+	buildForm() {
+		this.topicwiseforForm = this.fbuild.group({
+			tw_teacher_id: '',
+			teacher_name: '',
+			tw_class_id: '',
+			tw_sub_id: ''
+		});
+	}
+	getTeacherInfo(event) {
+		console.log(event.target.value);
+		this.teacherId = event.target.value;
+		this.axiomService.getAllTeacher({login_id: event.target.value, role_id: '3', status: '1'}).subscribe((result: any) => {
+			if (result && result.status === 'ok') {
+				console.log(result.data);
+				this.topicwiseforForm.patchValue({
+					teacher_name : result.data[0].au_full_name
+				});
+			} else {
+				this.commonAPIService.showSuccessErrorMessage(result.data, 'error');
+				this.topicwiseforForm.patchValue({
+					tw_teacher_id: '',
+					teacher_name : ''
+				});
+			}
+		});
+	}
+	getClassByTeacherId() {
+		this.classArray = [];
+		this.classSectionArray = [];
+		this.smartService.getClassByTeacherId({ teacher_id: this.teacherId}).subscribe(
+			(result: any) => {
+				if (result && result.status === 'ok') {
+					this.classArray = result.data;
+					if (this.classArray.length > 0 ) {
+						this.classArray.forEach(item => {
+							this.smartService.getSectionByTeacherIdClassId({
+								teacher_id: this.teacherId,
+								class_id: item.class_id
+							}).subscribe((result1: any) => {
+								if (result1 && result1.status === 'ok') {
+									this.sectionArray = result1.data;
+									if (this.sectionArray.length > 0) {
+										this.sectionArray.forEach(item2 => {
+											this.classSectionArray.push({
+												cs_id: item.class_id + '-' + item2.sec_id,
+												cs_name: item.class_name + ' - ' + item2.sec_name
+											});
+										});
+									}
+								}
+							});
+						});
+					}
+				} else {
+					this.commonAPIService.showSuccessErrorMessage(result.message, 'error');
+				}
+			});
+	}
+	getSubjectByTeacherIdClassIdSectionId(event) {
+		const csArray = event.value.split('-');
+		console.log(csArray);
+		this.subjectArray = [];
+
+		this.smartService.getSubjectByTeacherIdClassIdSectionId({ teacher_id: this.teacherId,
+			class_id: csArray[0], sec_id: csArray[1] }).subscribe((result: any) => {
+			if (result && result.status === 'ok') {
+				this.subjectArray = result.data;
+			} else {
+				// this.commonAPIService.showSuccessErrorMessage(result.message, 'error');
+			}
+		});
+
+	}
+
+	getTopicwise() {
+		console.log('calling gettopicwise');
+	}	
 
 }
