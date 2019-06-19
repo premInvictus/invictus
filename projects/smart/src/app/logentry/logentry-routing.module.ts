15274955f5083f6caa1446da5d622c4ce1080049
shadmanwaris <shadman@invictusdigisoft.com>
2019-06-19 18:50:31 +0530

    ui updated

--- projects/smart/src/app/logentry/logentry-routing.module.ts
+++ projects/smart/src/app/logentry/logentry-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClassworkUpdateComponent } from '../logentry/classwork-update/classwork-update.component';
 import { TopicwiseUpdateComponent } from '../logentry/topicwise-update/topicwise-update.component';
+import { ViewClassworkComponent } from './view-classwork/view-classwork.component';
 const routes: Routes = [
 	{
 		path: 'classwork-update', component: ClassworkUpdateComponent,
 	},
 	{
 		path: 'topicwise-update', component: TopicwiseUpdateComponent,
+	},
+	{
+		path: 'view-classwork', component: ViewClassworkComponent,
 	}
+
 ];
 
 @NgModule({
