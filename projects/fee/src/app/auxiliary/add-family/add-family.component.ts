b37bedb84e910af3a9ef3d004c896c101d30e904
shadmanwaris <shadman@invictusdigisoft.com>
2019-08-09 12:42:25 +0530

    ui updated

---
+++ projects/fee/src/app/auxiliary/add-family/add-family.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-family',
+  templateUrl: './add-family.component.html',
+  styleUrls: ['./add-family.component.css']
+})
+export class AddFamilyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
