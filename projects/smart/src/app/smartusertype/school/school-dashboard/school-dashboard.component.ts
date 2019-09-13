de812720a3ee94bd660114677e42d53355021fd0
User <shikhar.sagar@invictusdigisoft.com>
2019-09-13 18:41:11 +0530

    change cr

--- projects/smart/src/app/smartusertype/school/school-dashboard/school-dashboard.component.ts
+++ projects/smart/src/app/smartusertype/school/school-dashboard/school-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-school-dashboard',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SchoolDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
-
+  viewSyllabus() {
+    this.router.navigate(['smart/school/syllabus/browse']);
+  }
 }
