c4511c963b6d752d1487273d024b4b1a586ab597
shashank <shashank@invictusdigisoft.com>
2019-10-23 17:01:18 +0530

    hr patch value of first three tab

--- projects/human-resource/src/app/employee-master/employee-tab-three-container/employee-tab-three-container.component.ts
+++ projects/human-resource/src/app/employee-master/employee-tab-three-container/employee-tab-three-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, OnChanges } from '@angular/core';
+import { Component, OnInit, ViewChild, OnChanges, Input } from '@angular/core';
 import { SisService, CommonAPIService } from '../../_services/index';
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { saveAs } from 'file-saver';
@@ -10,10 +10,7 @@ import { ConfirmValidParentMatcher } from '../../ConfirmValidParentMatcher';
 })
 export class EmployeeTabThreeContainerComponent implements OnInit, OnChanges {
 	confirmValidParentMatcher = new ConfirmValidParentMatcher();
-	@ViewChild('general_remark') general_remark;
-	@ViewChild('management_remark') management_remark;
-	@ViewChild('admission_remark') admission_remark;
-	
+	@Input() employeedetails;
 	addOnly = false;
 	editOnly = false;
 	viewOnly = true;
@@ -39,6 +36,43 @@ export class EmployeeTabThreeContainerComponent implements OnInit, OnChanges {
 	}
 	buildForm() {
 		this.salaryDetails = this.fbuild.group({
+			pan: this.employeedetails.emp_salary_detail.account_docment_detail.pan_no,
+			aadhar: this.employeedetails.emp_salary_detail.account_docment_detail.aadhar_no,
+			pf_ac: this.employeedetails.emp_salary_detail.account_docment_detail.pf_acc_no,
+			esi_ac: this.employeedetails.emp_salary_detail.account_docment_detail.esi_acc_no,
+			nominee: this.employeedetails.emp_salary_detail.nominee_detail.name,
+			doj: this.employeedetails.emp_salary_detail.emp_organisation_relation_detail.doj,
+			pf_doj: this.employeedetails.emp_salary_detail.emp_organisation_relation_detail.pf_joining_date,
+			esi_doj: this.employeedetails.emp_salary_detail.emp_organisation_relation_detail.esic_joining_date,
+			probation: this.employeedetails.emp_salary_detail.emp_organisation_relation_detail.probation_till_date,
+			confirm_date: this.employeedetails.emp_salary_detail.emp_organisation_relation_detail.confirmation_date,
+			category_1: this.employeedetails.emp_salary_detail.emp_job_detail.category_1.cat_id,
+			category_2: this.employeedetails.emp_salary_detail.emp_job_detail.category_2.cat_id,
+			category_3: this.employeedetails.emp_salary_detail.emp_job_detail.category_3.cat_id,
+			increment_month: this.employeedetails.emp_salary_detail.emp_incremental_month_detail.month_data,
+			contract_period: this.employeedetails.emp_salary_detail.emp_job_detail.contact_period,
+			bank_name: this.employeedetails.emp_salary_detail.emp_bank_detail[0].bnk_detail.bnk_id,
+			bank_ac: this.employeedetails.emp_salary_detail.emp_bank_detail[0].bnk_detail.bnk_acc_no,
+			ifsc_code: this.employeedetails.emp_salary_detail.emp_bank_detail[0].bnk_detail.bnk_ifsc,
+			sal_str: this.employeedetails.emp_salary_detail.emp_bank_detail[0].bnk_detail.bnk_name,
+			pay_mode: '',
+			basic_pay: '',
+			da: '',
+			hra: '',
+			allowances: '',
+			pf_deduction: '',
+			esi_deduction: '',
+			tds_deduction: '',
+			net_salary: this.employeedetails.emp_salary_detail.emp_total_earning.emp_net_salary,
+			total_deduction: this.employeedetails.emp_salary_detail.emp_total_earning.emp_total_earning,
+		});
+	}
+	ngOnChanges() {
+		// this.getRemarkData(this.login_id);
+	}
+
+	getSalartDetails() {
+		this.salaryDetails.patchValue({
 			pan: '',
 			aadhar: '',
 			pf_ac: '',
@@ -70,10 +104,6 @@ export class EmployeeTabThreeContainerComponent implements OnInit, OnChanges {
 			total_deduction: ''
 		});
 	}
-	ngOnChanges() {
-		// this.getRemarkData(this.login_id);
-	}
-
 	saveForm() {
 		this.salaryDetailsArray['emp_salary_detail'] = {
 			account_docment_detail: {
@@ -89,7 +119,8 @@ export class EmployeeTabThreeContainerComponent implements OnInit, OnChanges {
 				doj: this.salaryDetails.value.doj,
 				pf_joining_date: this.salaryDetails.value.pf_doj,
 				esic_joining_date: this.salaryDetails.value.esi_doj,
-				probation_till_date: this.salaryDetails.value.probation
+				probation_till_date: this.salaryDetails.value.probation,
+				confirmation_date: this.salaryDetails.value.confirm_date
 			},
 			emp_job_detail: {
 				category_1: {
@@ -107,10 +138,7 @@ export class EmployeeTabThreeContainerComponent implements OnInit, OnChanges {
 				contact_period: this.salaryDetails.value.contract_period
 			},
 			emp_incremental_month_detail: {
-				month_data: {
-					month_id: this.salaryDetails.value.increment_month,
-					month_name: this.salaryDetails.value.increment_month
-				}
+				month_data: this.salaryDetails.value.increment_month
 			},
 			emp_bank_detail: [
 				{
