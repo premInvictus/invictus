ae5b3cf7173c8805f92f81f9a21e4a51f303dfc3
User <shikhar.sagar@invictusdigisoft.com>
2019-11-22 10:36:24 +0530

    changes id card printing

--- projects/sis/src/app/auxillary-tools/id-card-printing/id-card-printing.component.ts
+++ projects/sis/src/app/auxillary-tools/id-card-printing/id-card-printing.component.ts
@@ -248,7 +248,9 @@ export class IdCardPrintingComponent implements OnInit, AfterViewInit {
 		});
 	}
 	getIdCardSettings() {
-		this.sisService.getIdCardPrintSettings().subscribe((result: any) => {
+		this.sisService.getIdCardPrintSettings({
+			user_type: 'student'
+		}).subscribe((result: any) => {
 			if (result.status === 'ok') {
 				this.idCardSettings = result.data[0];
 			}
