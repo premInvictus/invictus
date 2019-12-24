406b24049a9a16640d822d355ee964e5d31d39eb
shashank <shashank@invictusdigisoft.com>
2019-12-24 14:48:40 +0530

    generate gate pass pdf

--- projects/inventory/src/app/store-master/goods-receipt/goods-receipt.component.ts
+++ projects/inventory/src/app/store-master/goods-receipt/goods-receipt.component.ts
@@ -5,7 +5,7 @@ import { Element } from './goods-receipt.model';
 import { CommonAPIService, SisService, AxiomService, InventoryService } from '../../_services';
 import { MatTableDataSource, MatPaginator, PageEvent } from '@angular/material';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-goods-receipt',
@@ -26,12 +26,13 @@ export class GoodsReceiptComponent implements OnInit {
   currentUser: any;
   session: any;
   ELEMENT_DATA: any[] = [];
+  schoolInfo: any;
   pageLength: number;
   pageSize = 300;
   pageSizeOptions = [100, 300, 1000];
   submitParam: any = {};
   displayedColumns: string[] = ['position', 'po_number', 'po_date', 'created_by', 'vendor_id', 'vendor_name',
-    'vendor_category', 'vendor_contact', 'vendor_email', 'action'];
+    'vendor_category', 'vendor_contact', 'vendor_email', 'print', 'action'];
   dataSource = new MatTableDataSource<Element>(this.ELEMENT_DATA);
   @ViewChild(MatPaginator) paginator: MatPaginator;
   spans = [];
@@ -51,7 +52,17 @@ export class GoodsReceiptComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getSchool();
     this.getAllOrderMaster();
+  }
+  getSchool() {
+    this.sisService.getSchool()
+      .subscribe(
+        (result: any) => {
+          if (result && result.status === 'ok') {
+            this.schoolInfo = result.data[0];
+          }
+        });
   }
   getAllOrderMaster() {
     this.ELEMENT_DATA = [];
@@ -71,6 +82,7 @@ export class GoodsReceiptComponent implements OnInit {
             "vendor_category": item.pm_vendor ? item.pm_vendor.ven_category : '-',
             "vendor_contact": item.pm_vendor ? item.pm_vendor.ven_contact : '-',
             "vendor_email": item.pm_vendor ? item.pm_vendor.ven_email : '-',
+            "print": { 'pm_id': item.pm_id, 'pm_status': item.pm_status },
             "action": { 'pm_id': item.pm_id, 'pm_status': item.pm_status }
           });
           ind++;
@@ -89,10 +101,25 @@ export class GoodsReceiptComponent implements OnInit {
       this.receiptModal.openModal(this.submitParam);
     }
   }
-  editList(pm_id) {
+  printReceipt(pm_id) {
     const sindex = this.orderArray.findIndex(f => Number(f.pm_id) === Number(pm_id));
     if (sindex !== -1) {
-      this.orderArray.splice(sindex, 1);
+      let receiptArray: any = {};
+      receiptArray['receipt_no'] = this.orderArray[sindex].pm_id;
+      receiptArray['date'] =this.commonService.dateConvertion(this.orderArray[sindex].pm_updated.update_date,'dd-MMM-y');
+      receiptArray['vendor_name'] = this.orderArray[sindex].pm_vendor.ven_name;
+      receiptArray['po_no'] = this.orderArray[sindex].pm_details.purchase_order_id;
+      receiptArray['invoice_no'] = this.orderArray[sindex].pm_details.invoice_no;
+      receiptArray['school_name'] = this.schoolInfo.school_name;
+      receiptArray['school_logo'] = this.schoolInfo.school_logo;
+      receiptArray['school_address'] = this.schoolInfo.school_address;
+      this.inventory.generatePdfOfReceipt(receiptArray).subscribe((result: any) => {
+        if (result && result.status == 'ok') {
+          this.commonService.showSuccessErrorMessage(result.message, 'success');
+          const length = result.data.fileUrl.split('/').length;
+          saveAs(result.data.fileUrl, result.data.fileUrl.split('/')[length - 1]);
+        }
+      });
     }
   }
   applyFilter(filterValue: string) {
