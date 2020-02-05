10458ca079a5a6ece9aa5bc28ad058bfe41da50c
shashank <shashank@invictusdigisoft.com>
2020-02-05 11:08:38 +0530

    call logs

---
+++ projects/miscellaneous/src/app/communication/call-logs/call-logs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-call-logs',
+  templateUrl: './call-logs.component.html',
+  styleUrls: ['./call-logs.component.css']
+})
+export class CallLogsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
