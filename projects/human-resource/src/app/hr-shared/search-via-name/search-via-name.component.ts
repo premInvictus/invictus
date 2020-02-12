1a5d1254d1ef969920e9c9af5737fce689217ca7
shashank <shashank@invictusdigisoft.com>
2020-02-12 18:47:46 +0530

    hr employee search and left

---
+++ projects/human-resource/src/app/hr-shared/search-via-name/search-via-name.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { SisService, CommonAPIService } from '../../_services/index';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-search-via-name',
+  templateUrl: './search-via-name.component.html',
+  styleUrls: ['./search-via-name.component.scss']
+})
+export class SearchViaNameComponent implements OnInit {
+  searchStudent = false;
+  shouldSizeUpdate: boolean;
+  studentArrayByName: any[] = [];
+  constructor(private CommonAPIService: CommonAPIService,
+    public dialogRef: MatDialogRef<SearchViaNameComponent>,
+    @Inject(MAT_DIALOG_DATA) data: any) {
+    this.shouldSizeUpdate = data.shouldSizeUpdate;
+  }
+
+  ngOnInit() {
+  }
+  searchStudentByName(value) {
+    if (value) {
+      const inputJson = {
+        "filters": [
+          {
+            "filter_type": "emp_name",
+            "filter_value": value,
+            "type": "text"
+          }
+        ]
+      };
+      this.CommonAPIService.getFilterData(inputJson).subscribe((result: any) => {
+        if (result && result.status === 'ok') {
+          console.log(result.data);
+          this.studentArrayByName = [];
+          this.studentArrayByName = result.data;
+          this.searchStudent = true;
+          this.updateSizeForData();
+          document.getElementById('search').blur();
+        } else {
+          this.searchStudent = true;
+          this.studentArrayByName = [];
+          this.updateSizeForNoData();
+          document.getElementById('search').blur();
+        }
+      });
+    }
+  }
+  setId(emp_id) {
+    this.dialogRef.close({ emp_id: emp_id });
+  }
+  closeDialog() {
+    this.dialogRef.close();
+  }
+  updateSizeForData() {
+    this.dialogRef.updateSize('60%', '60vh');
+  }
+  updateSizeForNoData() {
+    this.dialogRef.updateSize('60%', '40vh');
+  }
+
+
+}
