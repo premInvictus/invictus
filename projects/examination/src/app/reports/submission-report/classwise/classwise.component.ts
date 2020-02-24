aa655426059f6a5c51ab717eba31cc506085ed75
Bablu Shaw <bablushaw@invictusdigisoft.com>
2020-02-24 19:16:09 +0530

    types of submisstion and teacher name

--- projects/examination/src/app/reports/submission-report/classwise/classwise.component.ts
+++ projects/examination/src/app/reports/submission-report/classwise/classwise.component.ts
@@ -38,7 +38,7 @@ export class ClasswiseComponent implements OnInit, OnChanges {
     })
   }
   getReviewStatus(eme_class_id, eme_term_id,eme_sub_id,eme_sec_id,eme_exam_id,eme_subexam_id) {
-    status = '';
+    status = '-';
     if(this.printData.examMarkArr.length > 0) {
       this.printData.examMarkArr.forEach(element => {
         if(element.eme_class_id === eme_class_id && element.eme_term_id === eme_term_id && element.eme_sub_id === eme_sub_id
@@ -46,12 +46,30 @@ export class ClasswiseComponent implements OnInit, OnChanges {
             //status = element.eme_review_status;
             const tstatus = Number(element.eme_review_status);
             if(tstatus === 4) {
-              status = 'fas fa-thumbs-up text-published'
+              // status = 'fas fa-thumbs-up text-published'
+              //status = 'Published'
+              status = '<span class="text-published">Published</span>';
             } else if(tstatus > 0 && tstatus < 4) {
-              status = 'fas fa-check text-submitted';
+              //status = 'Submitted';
+              status = '<span class="text-submitted">Submitted</span>';
+            } else if(tstatus === 0) {
+              //status = 'Inprogress';
+              status = '<span class="text-inprogress">Inprogress</span>';
             }
           }
       });
+    }
+    if(status === '-') {
+      if(this.printData.subjectSubexamArr.length > 0) {
+        this.printData.subjectSubexamArr.forEach(element => {
+          if(element.ssm_class_id === eme_class_id && element.ssm_exam_id === eme_exam_id && element.ssm_se_id === eme_subexam_id && element.ssm_sub_id === eme_sub_id) {
+            if(Number(element.ssm_sub_mark) > 0) {
+              //status = 'Yet to start';
+              status = '<span class="text-yettostart">Yet to start</span>';
+            }
+          }
+        });
+      }
     }
     return status;
   }
@@ -63,6 +81,17 @@ export class ClasswiseComponent implements OnInit, OnChanges {
         name = element.eac_name;
       }
     });
+    return name;
+  }
+  getTeacherName(class_id,sub_id,sec_id){
+    let name = '';
+    if(this.printData.teacherArr.length > 0) {
+      this.printData.teacherArr.forEach(element => {
+        if(element.uc_class_id === class_id && element.uc_sec_id === sec_id && element.uc_sub_id === sub_id) {
+          name = element.au_full_name;
+        }
+      });
+    }
     return name;
   }
 
