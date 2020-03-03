06ebb202ae8e1b01375ae0fdf00678247fb42b58
Bablu Shaw <bablushaw@invictusdigisoft.com>
2020-03-03 11:38:13 +0530

    removed system info from library and hrms

--- projects/library-management/src/app/library-configure/library-configure-routing.module.ts
+++ projects/library-management/src/app/library-configure/library-configure-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SetupComponent } from '../library-configure/setup/setup.component';
-import { SystemInfoComponent } from '../library-configure/system-info/system-info.component';
 
 const routes: Routes = [
-  {path: 'setup', component: SetupComponent},
-  {path: 'system-info', component: SystemInfoComponent}
+  {path: 'setup', component: SetupComponent}
 ];
 
 @NgModule({
