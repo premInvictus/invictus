5df2691df3b4cd475140afca56b71ad946595e70
vishnubansal <vishnubansal@invictusdigisoft.com>
2020-03-18 19:13:39 +0530

    update for fines and penalities and fee transaction

--- projects/fee/src/app/feeconfiguration/fines-and-penalities/fines-and-penalities.component.ts
+++ projects/fee/src/app/feeconfiguration/fines-and-penalities/fines-and-penalities.component.ts
@@ -19,6 +19,7 @@ export class FinesAndPenalitiesComponent implements OnInit, AfterViewInit {
 	confirmValidParentMatcher = new ConfirmValidParentMatcher();
 	editFlag = false;
 	finepenaltiesForm: FormGroup;
+	monthWiseForm:any = FormGroup;
 	constructor(private fbuild: FormBuilder, private sisService: SisService,
 		private common: CommonAPIService,
 		private feeService: FeeService) { }
@@ -26,6 +27,22 @@ export class FinesAndPenalitiesComponent implements OnInit, AfterViewInit {
 	displayedColumns: string[] = ['srno', 'finename', 'finetype', 'fineamount', 'event', 'upperlimit', 'description', 'status', 'action'];
 	dataSource = new MatTableDataSource<FinePenaltiesElement>(this.FINEPENALTIES_ELEMENT_DATA);
 	fin_is_hostel_fee = 0;
+	chooseMonthLength = 0;
+	editMonthWiseData = [];
+	monthArr:any[] = [	{mon_id:1,mon_value:1},
+						{mon_id:2,mon_value:2},
+						{mon_id:3,mon_value:3},
+						{mon_id:4,mon_value:4},
+						{mon_id:5,mon_value:5},
+						{mon_id:6,mon_value:6},
+						{mon_id:7,mon_value:7},
+						{mon_id:8,mon_value:8},
+						{mon_id:9,mon_value:9},
+						{mon_id:10,mon_value:10},
+						{mon_id:11,mon_value:11},
+						{mon_id:12,mon_value:12}
+
+					 ]
 	@ViewChild('paginator') paginator: MatPaginator;
 	@ViewChild(MatSort) sort: MatSort;
 	btnDisable = false;
@@ -42,19 +59,52 @@ export class FinesAndPenalitiesComponent implements OnInit, AfterViewInit {
 			fin_id: '',
 			fin_name: '',
 			fin_type_id: '',
-			fin_amt: '',
+			fin_amt: 0,
 			fin_desc: '',
 			fin_max_month: '',
 			fin_class_id: '',
 			fin_sec_id: '',
 			fin_event_id: '',
 			fin_status: '1',
-			fin_upper_limit: ''
+			fin_upper_limit: '',
+			fin_no_of_month_selected:''
+			
 		});
 	}
 	ngAfterViewInit() {
 		this.dataSource.paginator = this.paginator;
 		this.dataSource.sort = this.sort;
+	}
+																									
+	renderMonthWiseFineControl(value) {
+		this.chooseMonthLength = value;
+		var temp_arr = [];
+
+		var diffLength = 0;
+		if (this.editMonthWiseData && this.editMonthWiseData.length) {
+			diffLength = value - this.editMonthWiseData.length;
+		} else {
+			diffLength = value;
+		}
+
+		if (this.editMonthWiseData && this.editMonthWiseData.length > 0) { 
+			for (let i=0; i<this.editMonthWiseData.length;i++) {
+				temp_arr.push(this.fbuild.group({
+					fin_month_fin_amt : this.editMonthWiseData[i]['month_data']['fine_amt'],
+					fin_month_days: this.editMonthWiseData[i]['month_data']['fine_days']
+	
+				}));
+			}
+		} 
+		for (let i=0; i<diffLength;i++) {
+			temp_arr.push(this.fbuild.group({
+				fin_month_fin_amt : '',
+				fin_month_days: ''
+
+			}));
+		}
+		this.monthWiseForm = this.fbuild.array(temp_arr);
+		console.log('chooseMonthLength--',this.chooseMonthLength, value, this.monthWiseForm );
 	}
 	getClass() {
 		this.sisService.getClass({}).subscribe((result: any) => {
@@ -129,6 +179,7 @@ export class FinesAndPenalitiesComponent implements OnInit, AfterViewInit {
 		});
 	}
 	submit() {
+		let monthWiseData = [];
 		if (!this.finepenaltiesForm.valid) {
 			this.btnDisable = false;
 			this.finepenaltiesForm.markAsDirty();
@@ -138,6 +189,16 @@ export class FinesAndPenalitiesComponent implements OnInit, AfterViewInit {
 			this.finepenaltiesForm.updateValueAndValidity();
 			this.finepenaltiesForm.value['fin_status'] = '1';
 			this.finepenaltiesForm.value['fin_is_hostel_fee'] = this.fin_is_hostel_fee;
+			if (this.monthWiseForm && this.monthWiseForm.value) {				
+				for (var i=0; i<this.monthWiseForm.value.length; i++) {
+					var monthJson =  {
+						"month_id" : i+1,
+						"month_data" : {"fine_amt" : this.monthWiseForm.value[i].fin_month_fin_amt , "fine_days" : this.monthWiseForm.value[i].fin_month_days}
+					};
+					monthWiseData.push(monthJson);
+				}
+			}
+			this.finepenaltiesForm.value['fin_month_wise_data'] = JSON.stringify(monthWiseData);
 			this.feeService.insertFineandPenalties(this.finepenaltiesForm.value).subscribe((result: any) => {
 				this.btnDisable = false;
 				if (result.status === 'ok') {
@@ -189,10 +250,30 @@ export class FinesAndPenalitiesComponent implements OnInit, AfterViewInit {
 			fin_sec_id: value.fin_sec_id,
 			fin_event_id: value.fin_event_id,
 			fin_status: value.fin_status,
-			fin_upper_limit: value.fin_upper_limit,
+			fin_upper_limit: value.fin_upper_limit,			
 		});
+
+		if (value && value.fin_month_wise_data) {
+			var monthWiseData = JSON.parse(value.fin_month_wise_data);
+			this.editMonthWiseData = JSON.parse(value.fin_month_wise_data);
+			this.finepenaltiesForm.patchValue({
+				fin_no_of_month_selected : monthWiseData.length
+			});			
+			this.renderMonthWiseFineControl(monthWiseData.length);
+			// var temp_arr = [];
+			// for (let i=0; i<monthWiseData.length;i++) {
+			// 	temp_arr.push(this.fbuild.group({
+			// 		fin_month_fin_amt : monthWiseData[i]['fine_amt'],
+			// 		fin_month_days: monthWiseData[i]['fine_days']
+	
+			// 	}));
+			// }
+			// this.monthWiseForm = this.fbuild.array(temp_arr);
+			// console.log('monthWiseForm--', this.monthWiseForm);
+		}
 	}
 	update() {
+		let monthWiseData = [];
 		if (!this.finepenaltiesForm.valid) {
 			this.btnDisable = false;
 			this.finepenaltiesForm.markAsDirty();
@@ -201,6 +282,17 @@ export class FinesAndPenalitiesComponent implements OnInit, AfterViewInit {
 			this.finepenaltiesForm.markAsPristine();
 			this.finepenaltiesForm.updateValueAndValidity();
 			this.finepenaltiesForm.value['fin_is_hostel_fee'] = this.fin_is_hostel_fee;
+			if (this.monthWiseForm && this.monthWiseForm.value) {				
+				for (var i=0; i<this.monthWiseForm.value.length; i++) {
+					var monthJson =  {
+						"month_id" : i+1,
+						"month_data" : {"fine_amt" : this.monthWiseForm.value[i].fin_month_fin_amt , "fine_days" : this.monthWiseForm.value[i].fin_month_days}
+					};
+					monthWiseData.push(monthJson);
+				}
+			}
+
+			this.finepenaltiesForm.value['fin_month_wise_data'] = JSON.stringify(monthWiseData);
 			this.feeService.insertFineandPenalties(this.finepenaltiesForm.value).subscribe((result: any) => {
 				this.btnDisable = false;
 				if (result.status === 'ok') {
@@ -229,6 +321,10 @@ export class FinesAndPenalitiesComponent implements OnInit, AfterViewInit {
 		this.getFinePenalties();
 	}
 	checkFineType(fine_id) {
+		this.chooseMonthLength = 0;
+		this.finepenaltiesForm.patchValue({
+			fin_no_of_month_selected : ''
+		})
 	}
 }
 
