949f21a100bbe9c4b244698459da9188ca79d659
Bablu Shaw <bablushaw@invictusdigisoft.com>
2020-03-18 15:40:28 +0530

    publish and unpublish remark

--- projects/examination/src/app/auxiliaries/gradecard-pagesetup/gradecard-pagesetup.component.ts
+++ projects/examination/src/app/auxiliaries/gradecard-pagesetup/gradecard-pagesetup.component.ts
@@ -14,7 +14,7 @@ export class GradecardPagesetupComponent implements OnInit {
   paramform: FormGroup;
   classArray: any[] = [];
   pageSize: any[] = [{id:'A4', name:'A4'},{id:'A3', name:'A3'}];
-  pageOreintation: any[] = [{id:'P', name:'Portrate'},{id:'L', name:'Landscape'}];
+  pageOreintation: any[] = [{id:'P', name:'Portrait'},{id:'L', name:'Landscape'}];
   fontSize: any[] = [5,6,7,8,9,10,11,12];
   pageMargin: any[] = [0,1,2,3,4,5,6,7,8,9,10,11,12];
   
