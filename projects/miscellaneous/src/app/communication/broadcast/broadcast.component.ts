fd9dff4001072b8eecece6657f4b172498ae4cf0
Bablu Shaw <bablushaw@invictusdigisoft.com>
2020-07-03 15:35:59 +0530

    pushnotification with composed list

--- projects/miscellaneous/src/app/communication/broadcast/broadcast.component.ts
+++ projects/miscellaneous/src/app/communication/broadcast/broadcast.component.ts
@@ -125,7 +125,14 @@ export class BroadcastComponent implements OnInit {
 		inputJson['to_date'] = new DatePipe('en-in').transform(this.broadcastForm.value.to_date, 'yyyy-MM-dd');
 		this.commonAPIService.getMessage(inputJson).subscribe((result: any) => {
 			if (result && result.data && result.data[0]) {
-				this.scheduleMessageData = result.data;
+				const tempresult = result.data;
+				tempresult.forEach(element => {
+					if(!(element.msg_to.length == 0 || element.msg_from == '')){
+						this.scheduleMessageData.push(element);
+					}
+				});
+				//this.scheduleMessageData = result.data;
+
 				this.prepareDataSource();
 			}
 		});
--- src/app/invictus-shared/top-nav/top-nav.component.ts
+++ src/app/invictus-shared/top-nav/top-nav.component.ts
@@ -510,7 +510,6 @@ export class TopNavComponent implements OnInit, OnDestroy, AfterViewInit {
 						if (event.notification_type.module === 'general') {
 							this.router.navigate(['student/notification'], { relativeTo: this.route });
 						}
-						this.router.navigate(['student/notification']);
 					}
 					if (this.currentUser.role_id === '3') {
 						if (event.notification_type.module === 'syllabus') {
@@ -530,6 +529,9 @@ export class TopNavComponent implements OnInit, OnDestroy, AfterViewInit {
 						}
 						if (event.notification_type.module === 'timetable') {
 							this.router.navigate(['../auxillaries/classwise-table'], { relativeTo: this.route });
+						}
+						if (event.notification_type.module === 'general') {
+							this.router.navigate(['teacher/notification'], { relativeTo: this.route });
 						}
 					}
 					if (this.currentUser.role_id === '2') {
