a9753a8ada3e2d7342433c5ce8894db26df96e8c
Amit khanna <amit.k@invictusdigisoft.com>
2021-04-16 22:27:03 +0530

    add for report card

--- projects/student-app/src/app/down-certificate/down-certificate.component.ts
+++ projects/student-app/src/app/down-certificate/down-certificate.component.ts
@@ -10,11 +10,15 @@ export class DownCertificateComponent implements OnInit {
   certificate_type_arr: any[] = [];
   currentUser: any = {};
   studentDetails: any = {};
+  gradeDetails: any = {};
+  settings: any[] = [];
+  showGradeCardFlag = false;
   constructor(private erp: ErpCommonService, private commonApiService: CommonAPIService) { }
 
   ngOnInit() {
 
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    
     this.getSlcTcTemplateSetting();
     this.getStudentDetails();
   }
@@ -60,11 +64,62 @@ export class DownCertificateComponent implements OnInit {
     })
 
   }
+
+  printGradecard() {
+    const printData: any[] = [];
+    printData.push(this.currentUser.login_id);
+    const param: any = {};
+    param.login_id = printData;
+    param.class_id = this.gradeDetails.class_id;
+    param.sec_id = (this.studentDetails.sec_id);
+    param.term_id = this.gradeDetails.term_id;
+    param.exam_id = this.gradeDetails.exam_id;
+    param.se_id = this.gradeDetails.subexam_id;
+    this.erp.printGradecard(param).subscribe((result: any) => {
+      if (result && result.status === 'ok') {
+        this.commonApiService.showSuccessErrorMessage('Download Successfully', 'success');
+        const length = result.data.split('/').length;
+        saveAs(result.data, result.data.split('/')[length - 1]);
+      } 
+    })
+
+  }
   getStudentDetails() {
+    this.getGlobalSetting();
     this.erp.getStudentInformation({ login_id: this.currentUser.login_id, enrollment_type: '4' }).subscribe((resutl1: any) => {
       if (resutl1 && resutl1.status === 'ok') {
         this.studentDetails = {};
         this.studentDetails = resutl1.data[0];
+console.log("in-studentDetails",this.studentDetails);
+        this.gradeDetails = {};
+          if (this.settings && this.settings.length > 0) {
+            const findex = this.settings.findIndex(f => Number(f.class_id) === Number(this.studentDetails.class_id)
+              && f.status === '1');
+              console.log("11",findex);
+            if (findex !== -1) {
+              if (this.settings[findex].sec_id && this.settings[findex].sec_id.length > 0) {
+                const findex2 = this.settings[findex].sec_id.findIndex(f => Number(f) === Number(this.studentDetails.sec_id));
+                if (findex2 !== -1) {
+                  this.showGradeCardFlag = true;
+                  this.gradeDetails = this.settings[findex];
+                  console.log("1",this.gradeDetails = this.settings[findex]);
+                }
+              } else {
+                this.showGradeCardFlag = true;
+                this.gradeDetails = this.settings[findex];
+                console.log("2",this.gradeDetails = this.settings[findex]);
+              }
+            }
+            console.log("22");
+          }
+      }
+    });
+  }
+  getGlobalSetting() {
+    this.erp.getGlobalSetting({ 'gs_alias': 'gradecard_report_settings_app' }).subscribe((result: any) => {
+      if (result && result.status === 'ok') {
+        this.settings = (result.data[0] && result.data[0]['gs_value']) ? JSON.parse(result.data[0] && result.data[0]['gs_value']) : [];
+      console.log("fun",this.settings);
       }
     });
   }
